1976b2154d3fdd653c8dcc6c36a50648ea54723f
c28am <c28am@dalton.org>
2024-12-04 09:03:18 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	TeamCode/src/main/java/BasicOmniOpMode_Linear.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderAuto.java
@@ -27,12 +27,14 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.robotcontroller.external.samples;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 /*
@@ -61,14 +63,14 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
 
-@Autonomous(name="Encoder Testing", group="Robot")
-public class EncoderAuto extends LinearOpMode {
+@TeleOp(name="Robot: Auto Drive By Encoder", group="Robot")
+public class RobotAutoDriveByEncoder_Linear extends LinearOpMode {
 
     /* Declare OpMode members. */
-    private DcMotor vertSlide = null;
-    private DcMotor horizontalSlide = null;
+    private DcMotor         leftDrive   = null;
+    private DcMotor         rightDrive  = null;
 
-    private ElapsedTime runtime = new ElapsedTime();
+    private ElapsedTime     runtime = new ElapsedTime();
 
     // Calculate the COUNTS_PER_INCH for your specific drive train.
     // Go to your motor vendor website to determine your motor's COUNTS_PER_MOTOR_REV
@@ -76,155 +78,111 @@ public class EncoderAuto extends LinearOpMode {
     // For example, use a value of 2.0 for a 12-tooth spur gear driving a 24-tooth spur gear.
     // This is gearing DOWN for less speed and more torque.
     // For gearing UP, use a gear ratio less than 1.0. Note this will affect the direction of wheel rotation.
-    static final double COUNTS_PER_MOTOR_REV = 537.7;    // eg: TETRIX Motor Encoder
-    static final double DRIVE_GEAR_REDUCTION = 1.0;     // No External Gearing.
-    static final double WHEEL_DIAMETER_INCHES = 4.0;     // For figuring circumference
-    static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+    static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
+    static final double     DRIVE_GEAR_REDUCTION    = 1.0 ;     // No External Gearing.
+    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
             (WHEEL_DIAMETER_INCHES * 3.1415);
-    static final double DRIVE_SPEED = 0.6;
-    static final double TURN_SPEED = 0.5;
+    static final double     DRIVE_SPEED             = 0.6;
+    static final double     TURN_SPEED              = 0.5;
 
     @Override
     public void runOpMode() {
 
         // Initialize the drive system variables.
-        vertSlide = hardwareMap.get(DcMotor.class, "vertSlide");
-        horizontalSlide = hardwareMap.get(DcMotor.class, "horizontalSlide");
+        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
+        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
 
         // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
         // When run, this OpMode should start both motors driving forward. So adjust these two lines based on your first test drive.
         // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
-        vertSlide.setDirection(DcMotor.Direction.REVERSE);
-        horizontalSlide.setDirection(DcMotor.Direction.REVERSE);
+        leftDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightDrive.setDirection(DcMotor.Direction.FORWARD);
 
-        vertSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        horizontalSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        vertSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        horizontalSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         // Send telemetry message to indicate successful Encoder reset
-        telemetry.addData("Starting at", vertSlide.getCurrentPosition());
-//                horizontalSlide.getCurrentPosition());
+        telemetry.addData("Starting at",  "%7d :%7d",
+                leftDrive.getCurrentPosition(),
+                rightDrive.getCurrentPosition());
         telemetry.update();
 
-        // Wait for the game to start (driver presses START)
+        // Wait for the game to start (driver presses PLAY)
         waitForStart();
 
         // Step through each leg of the path,
         // Note: Reverse movement is obtained by setting a negative distance (not speed)
-//        encoderDrive(DRIVE_SPEED, -24, 24, 5.0);  // S1: Forward 47 Inches with 5 Sec timeout
-//        encoderDrive(TURN_SPEED,   6, -6, 4.0);  // S2: Turn Right 12 Inches with 4 Sec timeout
-//        encoderDrive(DRIVE_SPEED, -12, -12, 4.0);  // S3: Reverse 24 Inches with 4 Sec timeout
+        encoderDrive(DRIVE_SPEED,  48,  48, 5.0);  // S1: Forward 47 Inches with 5 Sec timeout
+        encoderDrive(TURN_SPEED,   12, -12, 4.0);  // S2: Turn Right 12 Inches with 4 Sec timeout
+        encoderDrive(DRIVE_SPEED, -24, -24, 4.0);  // S3: Reverse 24 Inches with 4 Sec timeout
 
-//        telemetry.addData("Path", "Complete");
-//        telemetry.update();
-//        sleep(1000);  // pause to display final telemetry message.
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+        sleep(1000);  // pause to display final telemetry message.
+    }
 
-
-        /*
-         *  Method to perform a relative move, based on encoder counts.
-         *  Encoders are not reset as the move is based on the current position.
-         *  Move will stop if any of three conditions occur:
-         *  1) Move gets to the desired position
-         *  2) Move runs out of time
-         *  3) Driver stops the OpMode running.
-         */
-//    public void encoderDrive(double speed,
-//                             double leftInches, double rightInches,
-//                             double timeoutS) {
-//        int newLeftTarget;
-//        int newRightTarget;
+    /*
+     *  Method to perform a relative move, based on encoder counts.
+     *  Encoders are not reset as the move is based on the current position.
+     *  Move will stop if any of three conditions occur:
+     *  1) Move gets to the desired position
+     *  2) Move runs out of time
+     *  3) Driver stops the OpMode running.
+     */
+    public void encoderDrive(double speed,
+                             double leftInches, double rightInches,
+                             double timeoutS) {
+        int newLeftTarget;
+        int newRightTarget;
 
         // Ensure that the OpMode is still active
-        while (opModeIsActive()) {
+        if (opModeIsActive()) {
 
-//            // Determine new target position, and pass to motor controller
-//            newLeftTarget = vertSlide.getCurrentPosition() + (int) (leftInches * COUNTS_PER_INCH);
-//            newRightTarget = horizontalSlide.getCurrentPosition() + (int) (rightInches * COUNTS_PER_INCH);
-//            vertSlide.setTargetPosition(newLeftTarget);
-//            horizontalSlide.setTargetPosition(newRightTarget);
-//
-//            // Turn On RUN_TO_POSITION
-//            vertSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//            horizontalSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//
-//            // reset the timeout time and start motion.
-//            runtime.reset();
-//            vertSlide.setPower(Math.abs(speed));
-//            horizontalSlide.setPower(Math.abs(speed));
-//
-//            // keep looping while we are still active, and there is time left, and both motors are running.
-//            // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
-//            // its target position, the motion will stop.  This is "safer" in the event that the robot will
-//            // always end the motion as soon as possible.
-//            // However, if you require that BOTH motors have finished their moves before the robot continues
-//            // onto the next step, use (isBusy() || isBusy()) in the loop test.
-//            while (opModeIsActive() &&
-//                    (runtime.seconds() < timeoutS) &&
-//                    (vertSlide.isBusy() && horizontalSlide.isBusy())) {
-//
-//                // Display it for the driver.
-//                telemetry.addData("Running to", " %7d :%7d", newLeftTarget, newRightTarget);
-//                telemetry.addData("Currently at", " at %7d :%7d",
-//                        vertSlide.getCurrentPosition(), horizontalSlide.getCurrentPosition());
-//                telemetry.update();
-//            }
-//
-//            // Stop all motion;
-//            vertSlide.setPower(0);
-//            horizontalSlide.setPower(0);
-//
-//            // Turn off RUN_TO_POSITION
-//            vertSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-//            horizontalSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-//
-//            sleep(250);   // optional pause after each move.
+            // Determine new target position, and pass to motor controller
+            newLeftTarget = leftDrive.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
+            newRightTarget = rightDrive.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
+            leftDrive.setTargetPosition(newLeftTarget);
+            rightDrive.setTargetPosition(newRightTarget);
 
-//            if (gamepad2.left_trigger >= .95){ // slides move in
-//                horizontalSlide.setPower(-.95);
-//                telemetry.addData("Horizontal Encoder Count", horizontalSlide.getCurrentPosition());
-//                telemetry.update();
-//            } else {
-//                horizontalSlide.setPower(0);
-//            }
-//
-//            if (gamepad2.right_trigger >= .95){ // slides go out
-//                horizontalSlide.setPower(.95);
-//
-//            } else {
-//                horizontalSlide.setPower(0);
-//            }
-//            //encoder limits(WORKS)
-//            if(horizontalSlide.getCurrentPosition()< 1250 && gamepad2.left_trigger >= .95){
-//                horizontalSlide.setPower(.95);
-//                telemetry.addData("Horizontal Encoder Count", horizontalSlide.getCurrentPosition());
-//                telemetry.update();
-//            } else {
-//                horizontalSlide.setPower(0);
-//            }
-//            if(horizontalSlide.getCurrentPosition()> 0 && gamepad2.right_trigger >= .95){
-//                horizontalSlide.setPower(-.95);
-//                telemetry.addData("Horizontal Encoder Count", horizontalSlide.getCurrentPosition());
-//                telemetry.update();
-//            } else {
-//                horizontalSlide.setPower(0);
-//            }
+            // Turn On RUN_TO_POSITION
+            leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
+            // reset the timeout time and start motion.
+            runtime.reset();
+            leftDrive.setPower(Math.abs(speed));
+            rightDrive.setPower(Math.abs(speed));
 
-            if (gamepad2.b && vertSlide.getCurrentPosition()<5000) {
-                vertSlide.setPower(-.95);
-                telemetry.addData("Vertical Encoder Count", vertSlide.getCurrentPosition());
+            // keep looping while we are still active, and there is time left, and both motors are running.
+            // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
+            // its target position, the motion will stop.  This is "safer" in the event that the robot will
+            // always end the motion as soon as possible.
+            // However, if you require that BOTH motors have finished their moves before the robot continues
+            // onto the next step, use (isBusy() || isBusy()) in the loop test.
+            while (opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    (leftDrive.isBusy() && rightDrive.isBusy())) {
+
+                // Display it for the driver.
+                telemetry.addData("Running to",  " %7d :%7d", newLeftTarget,  newRightTarget);
+                telemetry.addData("Currently at",  " at %7d :%7d",
+                        leftDrive.getCurrentPosition(), rightDrive.getCurrentPosition());
                 telemetry.update();
-            } else {
-                vertSlide.setPower(0);
             }
-            if (gamepad2.a && vertSlide.getCurrentPosition()>0) {
-                vertSlide.setPower(.95);
 
-            } else {
-                vertSlide.setPower(0);
-            }
+            // Stop all motion;
+            leftDrive.setPower(0);
+            rightDrive.setPower(0);
+
+            // Turn off RUN_TO_POSITION
+            leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+            sleep(250);   // optional pause after each move.
         }
     }
 }
